817c950ce3b49275b923e694edccbb8a9f605f38
Okada Shohei <okashoicircus0@gmail.com>
2023-11-14 11:47:22 +0900

    Hyper の plugins から hyper-statusline を削除

--- .hyper.js
+++ .hyper.js
@@ -165,7 +165,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    "hyper-statusline",
+    // "hyper-statusline",
     "hyperterm-material",
     "hyper-search",
     "hypercwd",
